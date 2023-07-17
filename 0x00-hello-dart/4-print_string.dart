94c043cdee617b049cd57c6addb8f597411672a8
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-17 11:25:33 +0200

    Decrease code length

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,7 +1,5 @@
 void main() {
   String str = "Holberton School";
-  String result = str * 3;
-  String firstCharacters = str.substring(0,9);
-  print('$result');
-  print('$firstCharacters\n');
+  print(str * 3);
+  print(str.substring(0,9) + '\n');
 }
