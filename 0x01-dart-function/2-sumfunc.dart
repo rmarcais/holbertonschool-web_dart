594591fea55b6045ee2f078e4533aadf7871d0b7
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-18 12:48:29 +0200

    Format output

--- 0x01-dart-function/2-sumfunc.dart
+++ 0x01-dart-function/2-sumfunc.dart
@@ -7,8 +7,8 @@ int sub(int a, int b) {
 }
 
 String showFunc(int a, int b) {
-    String sum = "$a + $b = ${add(a, b)}";
-    String diff = "$a - $b = ${sub(a, b)}";
+    String sum = "Add $a + $b = ${add(a, b)}";
+    String diff = "Sub $a - $b = ${sub(a, b)}";
 
     return "$sum\n$diff";
 }
