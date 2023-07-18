e6307953d7120ad70ffb2a86d927b2321142a2a2
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-18 13:29:16 +0200

    Fix bug

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,6 +1,6 @@
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
-    final scoreA = (teamA['Free throws'] ?? 0) + (teamA['2 pointer'] ?? 0) * 2 + (teamA['3 pointer'] ?? 0) * 3;
-    final scoreB = (teamB['Free throws'] ?? 0) + (teamB['2 pointer'] ?? 0) * 2 + (teamB['3 pointer'] ?? 0) * 3;
+    final scoreA = (teamA['Free throws'] ?? 0) + (teamA['2 pointers'] ?? 0) * 2 + (teamA['3 pointers'] ?? 0) * 3;
+    final scoreB = (teamB['Free throws'] ?? 0) + (teamB['2 pointers'] ?? 0) * 2 + (teamB['3 pointers'] ?? 0) * 3;
 
     if (scoreA > scoreB) {
         return 1;
