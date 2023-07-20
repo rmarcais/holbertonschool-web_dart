343609f1aba2e8993ed242e70d0301bd5ab77d67
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-20 16:16:55 +0200

    Implement fromJson method

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,30 @@
+class User {
+  String? name;
+  int? age;
+  double? height;
+  int? id;
+
+  User({this.id, this.name, this.age, this.height});
+
+  Map<String, dynamic> toJson() {
+    return {
+        'id': id,
+        'name': name,
+        'age': age,
+        'height': height
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return new User(
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height']);
+  }
+
+  @override
+  String toString() {
+    return "User(id : $id ,name: $name, age: $age, height: $height)";
+  }
+}
