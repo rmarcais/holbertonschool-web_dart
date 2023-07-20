b209fe041776dd245ac0a73e4fd24417801603ea
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-20 16:00:07 +0200

    Remove the use of setters and getters

--- 0x02-dart-classes/3-private_password.dart
+++ 0x02-dart-classes/3-private_password.dart
@@ -1,30 +1,22 @@
 class Password {
     String _password = "";
 
-    String get password {
-        return _password;
-    }
-
-    void set password(String password) {
+    Password({password}) {
         this._password = password;
     }
 
-    Password({password}) {
-        this.password = password;
-    }
-
     bool isValid() {
-        bool hasGoodLength = password.length >= 8 && password.length <= 16;
-        bool hasUppercase = password.contains(new RegExp(r'[A-Z]'));
-        bool hasLowercase = password.contains(new RegExp(r'[a-z]'));
-        bool hasDigits = password.contains(new RegExp(r'[0-9]'));
+        bool hasGoodLength = _password.length >= 8 && _password.length <= 16;
+        bool hasUppercase = _password.contains(new RegExp(r'[A-Z]'));
+        bool hasLowercase = _password.contains(new RegExp(r'[a-z]'));
+        bool hasDigits = _password.contains(new RegExp(r'[0-9]'));
 
         return hasGoodLength && hasUppercase && hasLowercase && hasDigits;
     }
 
     @override
     String toString() {
-        return ("Your Password is: $password");
+        return ("Your Password is: $_password");
     }
 }
 
