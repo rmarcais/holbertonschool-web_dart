cc7414f8ea830238eee9f14881c6e5cba3e8099a
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-25 00:26:07 +0200

    Go back to the previous version

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -3,7 +3,19 @@ import 'dart:convert';
 
 Future<double> calculateTotal() async {
     try {
-        return -1;
+        String userData = await fetchUserData();
+        String userId = jsonDecode(userData)["id"];
+
+        String userOrders = await fetchUserOrders(userId);
+        List<dynamic> userProducts = jsonDecode(userOrders);
+
+        double total = 0;
+        for (final product in userProducts) {
+            String productPrice = await fetchProductPrice(product);
+            total += jsonDecode(productPrice);
+        }
+
+        return total;
     } catch (error) {
         return -1;
     }
