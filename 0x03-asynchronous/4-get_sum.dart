6789545604845f0af8ab67bb6b020ccda0f908a1
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-24 14:54:28 +0200

    Add 4-get_sum.dart

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,22 @@
+import '4-util.dart';
+import 'dart:convert';
+
+Future<double> calculateTotal() async {
+    try {
+        String userData = await fetchUserData();
+        String userId = jsonDecode(userData)["id"];
+
+        String userOrders = await fetchUserOrders(userId);
+        final userProducts = jsonDecode(userOrders);
+
+        double total = 0;
+        for (final product in userProducts) {
+            String productPrice = await fetchProductPrice(product);
+            total += jsonDecode(productPrice);
+        }
+
+        return total;
+    } catch (error) {
+        return -1;
+    }
+}
